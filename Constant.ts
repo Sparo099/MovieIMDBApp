3700e282089430449aa51c6bb82eb6470331692a
Sparo099 <102909340+Sparo099@users.noreply.github.com>
2023-07-25 16:29:22 +0300

    Add files via upload

---
+++ Constant.ts
@@ -0,0 +1,7 @@
+const STUDENT_NAME = "Noor Swalha";
+const STUDENT_EMAIL = "noorswalha2000@gmail.com";
+const FACULTY = "Software Engineer";
+
+const USER_KEY = "USER";
+const API_KEY = "154b3acc";
+export { STUDENT_EMAIL, STUDENT_NAME, FACULTY, USER_KEY, API_KEY };
